c87ac63cec03014927b351d301c8e80a5c52e4d1
Jack Cusick <jack.cusick@joinpropel.com>
2025-01-29 13:55:47 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "sample-module" {
-  source  = "app.terraform.io/jmorgancusick/sample-module/aws"
-  version = "0.0.1"
+  source  = "app.terraform.io/jmorgancusick/sample-module/aws?ref=0.0.1"
   # insert required variables here
   bucket_name = "jmorgancusick-private-module-s3"
 }
